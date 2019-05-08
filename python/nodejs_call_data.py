48506a92f4e30df0cebb4f56b282fdeb0b4b85a4
dongbaram <dongbaram@naver.com>
2019-05-08 17:09:28 +0900

    Create nodejs_call_data.py

---
+++ python/nodejs_call_data.py
@@ -0,0 +1,19 @@
+import sys,json
+
+def read_in():      #처리 function
+    lines = sys.stdin.readlines()   #파라미터를 읽음
+    return json.loads(lines[0])     #첫줄을 리턴
+
+def main():         #main function
+    param_data = read_in()
+    arg1 = param_data["param1"]
+    arg2 = param_data["param2"]
+    #np_lines = np.array(lines)
+    #lines_sum = np.sum(np_lines)
+    result_ocr = arg1 + arg2 + "Result OCR "
+    #return_date = [{"param1":arg1},{"param1":arg2},{"return":result_ocr}]
+
+    print(result_ocr)
+
+if __name__ == "__main__":  #서버에서 호출시 파일을 호출하므로 main으로 동작
+    main()
