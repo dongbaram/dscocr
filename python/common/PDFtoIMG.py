bacaccff6c4f8fa593ada980b3bb2ab8968b5972
dongbaram <dongbaram@naver.com>
2019-05-09 15:17:50 +0900

    Update PDFtoIMG.py

--- python/common/PDFtoIMG.py
+++ python/common/PDFtoIMG.py
@@ -1,30 +1,26 @@
-import os,sys,json
-#,fitz
+import sys,json
 
-#pdf_path=os.path.join('D:', os.sep, 'Python', 'test.pdf')
-#pdf_path2=os.path.join('D:', os.sep, 'Python', 'test_out.pdf')
-
-#print(pdf_path)
-
-#jpg_path=os.path.join('D:', os.sep, 'Python', 'test_out.jpg')
-#png_path=os.path.join('D:', os.sep, 'Python', 'test_out.png')
 def read_in():      #처리 function
     lines = sys.stdin.readlines()   #파라미터를 읽음
     return json.loads(lines[0])     #첫줄을 리턴
 
 def main():         #main function
-    
-    param_data = read_in()
-    arg1 = param_data["param1"]
+
+
     try:
-        #arg1 = 'D:/Python/uploads/1557366588289_test.pdf'
-        pdf_path=  arg1
-        pdf_path2='./uploads/test_out.pdf'
+        param_data = read_in()
+        arg1 = param_data["param1"]
+        # arg1 = 'D:/Python/uploads/1557366588289_test.pdf'
+        pdf_path = arg1
+        pdf_path2 = './uploads/test_out.pdf'
+
+        # jpg_path='../uploads/test_out.jpg'
+        png_path = './uploads/test.png'
 
     except Exception as msg:
         print(msg)
     else:
-        print(os.getcwd())
+        print(arg1)
 
 if __name__ == "__main__":  #서버에서 호출시 파일을 호출하므로 main으로 동작
     main()
