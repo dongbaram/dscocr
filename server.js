7b5a2c0a7a68bcac224a80f4a2749932ba4661df
dongbaram <dongbaram@naver.com>
2019-05-09 15:11:15 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 var upfilename = "";
  
 //운영 linux------------------------------------
-var uploadpath = '/opt/app-root/src/uploads';    
-var pyfile1 = '/opt/app-root/src/python/nodejs_call_data.py';
-var pyfile_pdftoimg = '/opt/app-root/src/python/common/PDFtoIMG.py';
+var uploadpath = './uploads';    
+var pyfile1 = './python/nodejs_call_data.py';
+var pyfile_pdftoimg = './python/common/PDFtoIMG.py';
  
 /*
 //개발 로컬-------------------------------------
@@ -98,7 +98,8 @@ app.post('/dscocr',function(req,res) {
 //서버 파일 다운로드
 app.get('/filedownload',function(req,res) {
     console.log("download filename:"+req.query.filename);
-    res.send("download filename:"+req.query.filename);
+    //res.send("download filename:"+req.query.filename);
+    res.download(uploadpath + '/' + req.query.filename);
 });
 
 
