59eaff10bcfb2dc5c5edb97fe486787e38a70ad7
dongbaram <dongbaram@naver.com>
2019-05-09 14:44:09 +0900

    Update PDFtoIMG.py

--- python/common/PDFtoIMG.py
+++ python/common/PDFtoIMG.py
@@ -1,4 +1,5 @@
-import os,sys,json,fitz
+import os,sys,json
+#,fitz
 
 #pdf_path=os.path.join('D:', os.sep, 'Python', 'test.pdf')
 #pdf_path2=os.path.join('D:', os.sep, 'Python', 'test_out.pdf')
@@ -17,31 +18,7 @@ def main():         #main function
     #arg1 = 'D:/Python/uploads/1557366588289_test.pdf'
     pdf_path=  arg1
     pdf_path2='./uploads/test_out.pdf'
-
-    #jpg_path='../uploads/test_out.jpg'
-    png_path='./uploads/test.png'
-
-    try:
-        with open(pdf_path, "rb") as binary_file:
-            # Read the whole file at once
-            b_data = binary_file.read()
-            #print(b_data)
-
-        new_file = open(pdf_path2, "wb")
-        new_fileByteArray = bytearray(b_data)
-        new_file.write(new_fileByteArray)
-        new_file.close()
-
-        doc = fitz.open(pdf_path2)
-        page = doc.loadPage(0) #number of page
-        zoom_x = 2.0                       # horizontal zoom
-        zomm_y = 2.0                       # vertical zoom
-        mat = fitz.Matrix(zoom_x, zomm_y)  # zoom factor 2 in each dimension
-        pix = page.getPixmap(matrix = mat) # use 'mat' instead of the identity matrix
-        output = png_path
-        pix.writePNG(output)
-        #END def ufn_pdf_to_img(p_data)
-
+ 
     except Exception as msg:
         print(msg)
     else:
